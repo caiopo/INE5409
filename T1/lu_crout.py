93511d925fa55cc7859801c5f1a94f31c69dc607
caiopo <caiopoliveira@gmail.com>
2018-09-01 23:14:24 -0300

    Finish LU-Crout

--- T1/lu_crout.py
+++ T1/lu_crout.py
@@ -2,30 +2,13 @@ import numpy as np
 from operator import itemgetter
 
 
-# def swap_rows(k, A, b):
-#     n = len(A)
-
-#     max_index, max_value = max(
-#         enumerate(A[:, k][k:]),
-#         key=lambda t: abs(t[1]),
-#     )
-
-#     # print(f'mi: {max_index}, mv: {max_value}')
-
-#     A[[k, max_index]] = A[[max_index, k]]
-#     b[[k, max_index]] = b[[max_index, k]]
-
-
 def swap_rows(k, A, b):
     n = len(A)
 
-    max_value = abs(A[k, k])
-    max_index = k
-
-    for i in range(k + 1, n):
-        if abs(A[i, k]) > max_value:
-            max_value = abs(A[i, k])
-            max_index = i
+    max_value, max_index = max(
+        (abs(A[i, k]), i)
+        for i in range(k, n)
+    )
 
     A[[k, max_index]] = A[[max_index, k]]
     b[[k, max_index]] = b[[max_index, k]]
@@ -59,6 +42,35 @@ def decompose(A, b):
     return A, b
 
 
+def calc_x(A, b):
+    n = len(A)
+
+    C = np.zeros(n)
+    X = np.zeros(n)
+
+    C[0] = b[0] / A[0, 0]
+
+    for i in range(1, n):
+        sum_ = sum(A[i, :i] * C[:i])
+        C[i] = (b[i] - sum_) / A[i, i]
+
+    X[n - 1] = C[n - 1]
+
+    for i in range(n - 2, -1, -1):
+        sum_ = sum(A[i, i + 1:] * X[i + 1:])
+        X[i] = C[i] - sum_
+
+    return X
+
+
+def lu_crout(A, b):
+    A, b = decompose(A, b)
+
+    X = calc_x(A, b)
+
+    return X
+
+
 if __name__ == '__main__':
     A = np.array([
         [0, 1, 2],
@@ -66,13 +78,14 @@ if __name__ == '__main__':
         [1, 0, 1],
     ], dtype=float)
 
-    b = np.array([
-        [1],
-        [2],
-        [3],
-    ], dtype=float)
+    bs = [
+        np.array([1, 2, 3], dtype=float),
+        np.array([0, 1, 2], dtype=float),
+        np.array([3, 5, 7], dtype=float),
+        np.array([1, -1, 5], dtype=float),
+    ]
 
-    A, b = decompose(A, b)
+    for b in bs:
+        X = lu_crout(A, b)
 
-    print(A)
-    print(b)
+        print(X)
