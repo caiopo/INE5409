c4031e522430526a439364d65f0b59d2ec095020
caiopo <caiopoliveira@gmail.com>
2018-11-30 15:03:31 -0200

    P4 checkpoint

--- P4/main.m
+++ P4/main.m
@@ -5,7 +5,6 @@ f = @(x) log(x);
 a = 0.01;
 b = 1;
 
-disp('')
 disp('Questão 3')
 
 n = 2**18;
@@ -13,16 +12,57 @@ Tn = fIntegralTrapezios(n, a, b, f)
 erroExatoEstimadoTn = abs(Tn - fIntegralTrapezios(2*n, a, b, f))
 
 disp('')
+disp('')
 disp('Questão 4')
 
 n = 2**11;
 Sn = fIntegralSimpson(n, a, b, f)
 erroExatoEstimadoSn = abs(Sn - fIntegralSimpson(2*n, a, b, f))
 
-
+disp('')
 disp('')
 disp('Questão 5')
 
 m = 22;
 Gm = fIntegralGauss(m, a, b, f)
 erroExatoEstimadoGm = abs(Gm - fIntegralGauss(m+1, a, b, f))
+
+disp('')
+disp('')
+disp('Questão 7')
+
+m = 1e3;
+GTmExato = 1/pi * 2.4039394306344129982733248925915; % retirado do wolfram
+
+GTm = 1/pi * fGTm(m)
+
+erroGTm = abs(GTmExato - GTm)
+
+disp('')
+disp('')
+disp('Questão 8')
+
+x = [1 2 4 7 11 17 23 29 35 41];
+y = [4.3 3.9 3.3 2.9 2.5 2.1 1.8 1.5 1.4 1.2];
+
+xp = min(x) : 1/100 : max(x);
+
+n = 4;
+a1 = fCalculaCoefAjustePn(x, y, n)
+yp1 = fPn(n, a1, xp);
+
+disp('')
+disp('')
+disp('Questão 9')
+
+desviosPn = (fPn(n, a1, x) .- y).^2
+
+plt = plot(
+    x, y, '*k',
+    xp, yp1, '-r'
+);
+legend(
+    'Pontos originais',
+    'Ajuste polinomial'
+)
+waitfor(plt)
